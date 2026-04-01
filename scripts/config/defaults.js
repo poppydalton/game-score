b624ab62d77e491f7a2a4e2a6c383389ee51b6fe
Poppy Dalton <poppycdalton@gmail.com>
2026-04-01 14:38:25 -0500

    Change default player name and update leaderboard

--- scripts/config/defaults.js
+++ scripts/config/defaults.js
@@ -1,7 +1,7 @@
-export const DEFAULT_PLAYER_NAME = 'Poppy Dalton';
+export const DEFAULT_PLAYER_NAME = 'Poppy';
 
 export const DEFAULT_LEADERBOARD = [
-  { name: 'Poppy', score: 9000 },
+  { name: 'Garfield', score: 9000 },
   { name: 'Nermal', score: 7000 },
   { name: 'Odie', score: 5000 },
   { name: 'Arlene', score: 3000 }
